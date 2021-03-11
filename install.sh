9d6d9804960ee795cdecb4b28853e82484082d46
jannec22 <jannec1290@gmail.com>
2021-03-11 13:33:23 +0100

    changes in installation scripts

--- install.sh
+++ install.sh
@@ -2,19 +2,19 @@
 
 set -e
 
-RICE_DIR="${2:-"$(pwd)"}"
 DEST="${1:-"$HOME"}"
+RICE_DIR="${2:-"$(pwd)"}"
 
 [ ! -d "$RICE_DIR" ] && echo "$RICE_DIR does not point to archrice directory" && exit 1
 [ ! -d "$DEST" ] && echo "$DEST is not a valid directory" && exit 1
 
-cd "$DEST"
+echo "using RICE_DIR=$RICE_DIR, DEST=$DEST"
 
-[ -d old_config ] && echo "old_config exists. rename it and run again" && exit 1
+[ -d "$DEST/old_config" ] && echo "old_config exists. rename it and run again" && exit 1
 # rename old configs and local files
-mkdir old_config
+mkdir "$DEST/old_config"
 
 find "$RICE_DIR" -maxdepth 1 | while read el; do
-  [ -e "$DEST/$(basename "$el")" ] && mv "$DEST/$(basename "$el")" old_config
+  [ -e "$DEST/$(basename "$el")" ] && mv -i "$DEST/$(basename "$el")" old_config && echo "moved $DEST/$(basename "$el") to old_config"
   ln -s "$el" "$DEST/$(basename "$el")"
 done
--- uninstall.sh
+++ uninstall.sh
@@ -8,10 +8,8 @@ INSTALLATION_DIR="${1:-"$HOME"}"
 [ ! -d "$RICE_DIR" ] && echo "$RICE_DIR does not point to archrice directory" && exit 1
 [ ! -d "$INSTALLATION_DIR" ] && echo "$INSTALLATION_DIR is not a valid directory" && exit 1
 
-cd "$INSTALLATION_DIR"
-
-find . -maxdepth 1 -type l | while read link; do
-  [ readlink -f "$link" | grep "$INSTALLATION_DIR" ] && rm -i "$link"
+for link in $(find "$INSTALLATION_DIR" -maxdepth 1 -type l); do
+  readlink -f "$link" | grep "$RICE_DIR" && rm -i "$link"
 done
 
 find "$RICE_DIR" -maxdepth 1 | while read el; do
